7ba04883d7f15e11cc29d19fc0b396eeabb5f853
SHIINASAMA <SHIINA_KAORU@OUTLOOK.COM>
2020-12-09 18:59:51 +0800

    ZString 类

---
+++ Zlibx/ZString.cpp
@@ -0,0 +1,91 @@
+/**	@file	ZString.cpp
+*	@brief	×Ö·û´®ÀàÊµÏÖ
+*	@author	kaoru(SHIINA_KAORU@OUTLOOK.COM)
+*	@date	2020-12-09
+*/
+
+#include "ZString.h"
+
+ZString::ZString()
+{
+	//DO NOTHING
+}
+
+ZString::ZString(WString objStr)
+{
+	this->str = objStr;
+}
+
+ZString::ZString(AString objStr)
+{
+	Pause(objStr);
+}
+
+WString ZString::ToWString()
+{
+	return this->str;
+}
+
+AString ZString::ToAString()
+{
+	int nLen = WideCharToMultiByte(CP_ACP, 0, str, -1, NULL, 0, NULL, NULL);
+	if (nLen == 0)
+	{
+		return NULL;
+	}
+	char* pResult = new char[nLen];
+	WideCharToMultiByte(CP_ACP, 0, str, -1, pResult, nLen, NULL, NULL);
+	return pResult;
+}
+
+int ZString::Len()
+{
+	if (this->str == NULL)return -1;
+	WString str = this->str;
+	int len = 0;
+	while (*str++)
+	{
+		len++;
+	}
+	return len;
+}
+
+void ZString::Pause(WString srcStr)
+{
+	this->str = srcStr;
+}
+
+void ZString::Pause(AString srcStr)
+{
+	int nLen = MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, srcStr, -1, NULL, 0);
+	if (nLen == 0)
+	{
+		return;
+	}
+	wchar_t* pResult = new wchar_t[nLen];
+	MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, srcStr, -1, pResult, nLen);
+	this->str = pResult;
+}
+
+void ZString::operator=(WString str)
+{
+	this->str = str;
+}
+
+void ZString::operator=(WChar str[])
+{
+	this->str = str;
+}
+
+ZString::operator WString()
+{
+	return this->str;
+}
+
+WChar ZString::operator[](int index)
+{
+	WString str = this->str;
+	str += index;
+	WChar c = *str;
+	return c;
+}
---
+++ Zlibx/ZString.h
@@ -0,0 +1,67 @@
+/**	@file	ZString.h
+*	@brief	字符串类定义
+*	@author kaoru(SHIINA_KAORU@OUTLOOK.COM)
+*	@date	2020-12-09
+*/
+
+#pragma once
+
+#include <Windows.h>
+
+#define WString LPCWSTR
+#define WChar wchar_t
+#define AString char*
+#define AChar char
+
+/**	@brief ZString类定义
+*/
+class ZString
+{
+private:
+	WString str;
+
+public:
+	/** @brief 注意，这会创建一个空的ZString对象
+	*/
+	ZString();
+
+	/** @brief			利用WString初始化一个ZString对象
+	*	@param	objStr	目标WString对象
+	*/
+	ZString(WString objStr);
+
+	/** @brief			利用AString初始化一个ZString对象
+	*	@param	objStr	目标AString对象
+	*/
+	ZString(AString objStr);
+
+	/** @brief	得到ZString的WString对象
+	*	@retval 目标对象
+	*/
+	WString ToWString();
+
+	/** @brief	得到ZString的AString对象
+	*	@retval	目标对象
+	*/
+	AString ToAString();
+
+	/** @brief	ZString对象的字符串长度
+	*	@retval	字符串长度
+	*/
+	int Len();
+
+	/**	@brief			转换一个WString对象
+	*	@param	srcStr	源字符串
+	*/
+	void Pause(WString srcStr);
+
+	/**	@brief			转换一个AString对象
+	*	@param	srcStr	源字符串
+	*/
+	void Pause(AString srcStr);
+
+	void operator=(WString str);
+	void operator=(WChar str[]);
+	operator WString();
+	WChar operator[](int index);
+};
