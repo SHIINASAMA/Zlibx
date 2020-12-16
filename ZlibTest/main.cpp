6e45a07a32c9d8efcdf3b23176cd25f7ced4433a
SHIINASAMA <SHIINA_KAORU@OUTLOOK.COM>
2020-12-16 20:24:15 +0800

    ZProgressBar

--- ZlibTest/main.cpp
+++ ZlibTest/main.cpp
@@ -3,18 +3,20 @@
 #include "../Zlibx/ZLabel.h"
 #include "../Zlibx/ZButton.h"
 #include "../Zlibx/ZTextBox.h"
+#include "../Zlibx/ZProgressBar.h"
 
 #pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
 
 ZWindow* win;
 ZLabel* l1;
 ZButton* b1;
-ZTextBox* t1;
+ZProgressBar* pb1;
+
+UINT v = 0;
 
 LRESULT b1_clicked(Param param)
 {
-	ZString str = t1->GetText();
-	l1->SetText(str);
+	pb1->SetValue(++v);
 	return 0;
 }
 
@@ -23,15 +25,12 @@ int main()
 	win = new ZWindow(L"Hello", 50, 50, 300, 200, WindowType::Sizable);
 	win->Create();
 
-	l1 = new ZLabel(L"Hello", 5, 5, 100, 21);
-	win->AddControl(l1);
-
-	b1 = new ZButton(L"设置文本", 5, 55, 90, 50);
+	b1 = new ZButton(L"增加", 5, 55, 90, 50);
 	b1->Bind(MessageType::Clicked, b1_clicked);
 	win->AddControl(b1);
 
-	t1 = new ZTextBox(5, 30, 90, 21);
-	win->AddControl(t1);
+	pb1 = new ZProgressBar(5, 5, 300, 21);
+	win->AddControl(pb1);
 
 	win->Run();
 	return 0;
---
+++ Zlibx/ZProgressBar.h
@@ -0,0 +1,96 @@
+/**
+ * \file   ZProgressBar.h
+ * \brief	进度条控件类定义
+ *
+ * \author kaoru(SHIINA_KAORU@OUTLOOK.COM)
+ * \date   2020-12-16
+ */
+#pragma once
+#include "ZControl.h"
+#include "ZRange.h"
+
+ /**
+  * \brief 进度条类型
+  */
+enum class DLLAPI ProgressBarType
+{
+	/** 常规水平 */
+	Normal,
+	/** 常规平滑 */
+	Smooth,
+	/** 常规垂直 */
+	NormalV,
+	/** 平滑垂直 */
+	SmoothV
+};
+
+/** ZProgressBar 类定义 */
+class DLLAPI ZProgressBar : public ZControl
+{
+	UINT min;
+	UINT max;
+public:
+	/**
+	 * 实例化一个进度条控件对象
+	 *
+	 * \param x		x坐标
+	 * \param y		y坐标
+	 * \param w		宽
+	 * \param h		高
+	 * \param min	最小值
+	 * \param max	最大值
+	 * \param type	进度条类型
+	 */
+	ZProgressBar(int x, int y, int w, int h, UINT min = 0, UINT max = 100, ProgressBarType type = ProgressBarType::Normal);
+
+	/**
+	 * 设置控件风格
+	 *
+	 * \param type		控件样式
+	 * \deprecated		最好不要使用该函数
+	 */
+	void SetStyle(ProgressBarType type);
+
+	/**
+	 * 重写父控件调用的初始化函数
+	 *
+	 * \param hWnd 父控件句柄
+	 */
+	void Init(HWND hWnd);
+
+	/**
+	 * 返回当前的进度值
+	 *
+	 * \return 进度值
+	 */
+	UINT GetValue();
+
+	/**
+	 * 设置当前的进度值
+	 *
+	 * \param value
+	 */
+	void SetValue(UINT value);
+
+	/**
+	 * 获取进度区间
+	 *
+	 * \return 进度区间
+	 */
+	ZRange GetRange();
+
+	/**
+	 * 设置进度区间
+	 *
+	 * \param range 进度区间
+	 */
+	void SetRange(ZRange range);
+
+	/**
+	 * 设置进度区间
+	 *
+	 * \param min 左端点
+	 * \param max 右端点
+	 */
+	void SetRange(UINT min, UINT max);
+};
