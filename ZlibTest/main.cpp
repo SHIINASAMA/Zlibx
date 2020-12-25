f0406f847f3a184a153fccd217cf79381950e2d9
SHIINASAMA <SHIINA_KAORU@OUTLOOK.COM>
2020-12-25 21:39:25 +0800

    upgrade zcontrol

--- ZlibTest/main.cpp
+++ ZlibTest/main.cpp
@@ -5,9 +5,11 @@
 #include "../Zlibx/ZPictureBox.h"
 
 ZWindow* win;
+ZPictureBox* pb1;
 
 void btn_clicked(WPARAM wParam, LPARAM lParam)
 {
+	pb1->SetPosition(ZPoint{ 100,100 });
 }
 
 int main()
@@ -16,19 +18,19 @@ int main()
 	win->Create();
 	win->ShowCenter();
 
-	//ZButton* b1 = new ZButton(L"Hello", 5, 105, 100, 50);
-	//b1->Bind(btn_clicked);
-	////b1->SetEnable(FALSE);
-	//win->Add(b1);
+	ZButton* b1 = new ZButton(L"Hello", 5, 105, 100, 50);
+	b1->Bind(btn_clicked);
+	//b1->SetEnable(FALSE);
+	win->Add(b1);
 
-	ZLabel* l1 = new ZLabel(L"Hello", 5, 105, 100, 50);
-	win->Add(l1);
+	/*ZLabel* l1 = new ZLabel(L"Hello", 5, 105, 100, 50);
+	win->Add(l1);*/
 
-	auto pb1 = new ZPictureBox(5, 5, 100, 100, DisplayMode::Zoom);
+	pb1 = new ZPictureBox(5, 5, 100, 100, DisplayMode::Zoom);
+	win->Add(pb1);
 	ZBitmap bmp;
 	bmp.LoadFromFile(L"C:\\Users\\kaoru\\Desktop\\女子高生.bmp");
 	pb1->SetImage(bmp);
-	win->Add(pb1);
 
 	win->Run();
 }
--- Zlibx/ZControl.cpp
+++ Zlibx/ZControl.cpp
@@ -13,10 +13,76 @@ LRESULT ZControl::ConProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return LRESULT();
 }
 
+void ZControl::UpdateRect(ZControl* con)
+{
+	RECT rect;
+	GetWindowRect(con->hWnd, &rect);
+	con->rect = rect;
+}
+
 ZControl::~ZControl()
 {
 }
 
 ZControl::ZControl()
 {
+}
+
+void ZControl::SetPosition(ZPoint point)
+{
+	UpdateRect(this);
+	auto rect = this->rect.ToClientRect();
+	rect.OffsetRect(point.x, point.y);
+	MoveWindow(
+		hWnd,
+		rect.A.x,
+		rect.A.y,
+		rect.GetWidth(),
+		rect.GetHeight(),
+		TRUE
+	);
+}
+
+ZPoint ZControl::GetPosition()
+{
+	UpdateRect(this);
+	return rect.A;
+}
+
+void ZControl::SetWidth(UINT w)
+{
+	UpdateRect(this);
+	MoveWindow(
+		hWnd,
+		rect.A.x,
+		rect.A.y,
+		rect.A.x + w,
+		rect.B.y,
+		TRUE
+	);
+}
+
+UINT ZControl::GetWidth()
+{
+	UpdateRect(this);
+	return rect.GetWidth();
+}
+
+void ZControl::SetHeight(UINT h)
+{
+	UpdateRect(this);
+	MoveWindow(
+		hWnd,
+		rect.A.x,
+		rect.A.y,
+		rect.B.x,
+		rect.A.y + h,
+		TRUE
+	);
+}
+
+UINT ZControl::GetHeight()
+{
+	UpdateRect(this);
+	return rect.GetHeight();
 }
